8266f62ba99bf45639597648f888f22d245f3b56
Marco van Eerden <mvaneerden@tudelft.nl>
2022-12-01 23:28:45 +0100

    Oneliner day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,42 +1,7 @@
+extern crate itertools;
+use itertools::Itertools;
+
 fn main()
 {
-    let mut max: [u64; 3] = [0; 3];
-    let mut local_sum: u64 = 0;
-    let contents = std::fs::read_to_string(std::path::Path::new(&String::from("day1/input/input.txt")))
-        .expect("Error reading file.");
-
-    for line in contents.lines()
-    {
-        match line.parse::<u64>()
-        {
-            Ok(value) => local_sum += value,
-            Err(_) =>
-            {
-                if local_sum > max[1]
-                {
-                    if local_sum > max[0]
-                    {
-                        max[2] = max[1];
-                        max[1] = max[0];
-                        max[0] = local_sum
-                    }
-                    else
-                    {
-                        max[2] = max[1];
-                        max[1] = local_sum
-                    }
-                }
-                else if local_sum > max[2]
-                {
-                    max[2] = local_sum
-                }
-
-                local_sum = 0
-            }
-        }
-    }
-
-    let sum_total = max[0] + max[1] + max[2];
-
-    println!("{sum_total}");
+    println!("{}", std::fs::read_to_string(std::path::Path::new(&String::from("day1/input/input.txt"))).expect("Error reading file.").lines().map(| val | { val.parse::<i64>().unwrap_or(-1) }).coalesce(| a, b | if b >= 0 { Ok(a + b) } else { Err((a, b)) } ).sorted_by(| a, b | b.cmp(a)).take(3).sum::<i64>());
 }
