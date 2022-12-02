698d3c6cdc613eb08c0ae22e01237d3741659b9f
Marco van Eerden <mvaneerden@tudelft.nl>
2022-12-02 19:14:20 +0100

    Add day2

---
+++ day2/part2/src/main.rs
@@ -0,0 +1,4 @@
+fn main()
+{
+    println!("{}",std::fs::read_to_string(std::path::Path::new(&String::from("day2/input/input.txt"))).expect("Error reading file.").lines().map(|line|{(line.split(" ").take(2).nth(0).expect("Error:no 1st element!"),line.split(" ").take(2).nth(1).expect("Error: no 1st element!"))}).map(|chars|{return match chars.1{"X"=>if chars.0=="A"{3+0}else if chars.0=="B"{1+0}else{2+0},"Y"=>if chars.0=="A"{1+3}else if chars.0=="B"{2+3}else{3+3},"Z"=>if chars.0=="A"{2+6}else if chars.0=="B"{3+6}else{1+6},_=>panic!("Not possible ;)")}}).sum());
+}
